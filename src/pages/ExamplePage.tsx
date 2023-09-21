2d15e59c7593f3ba3fb05da715ad2f0267b59da3
Aaron Sanders <aaron.sanders@grafana.com>
2023-09-21 08:03:50 -0500

    Switch ExamplePage over to using hook

--- src/pages/ExamplePage.tsx
+++ src/pages/ExamplePage.tsx
@@ -1,67 +1,22 @@
 import React, { useState } from 'react';
-import { useAsync } from 'react-use';
 
 import { llms } from '@grafana/experimental';
 import { PluginPage } from '@grafana/runtime';
 import { Button, Input, Spinner } from '@grafana/ui';
-import { finalize } from 'rxjs';
-
+import { useOpenaiStreamHook } from '../hooks/hooks';
 export function ExamplePage() {
   // The current input value.
   const [input, setInput] = useState('');
-  // The final message to send to the LLM, updated when the button is clicked.
-  const [message, setMessage] = useState('');
-  // The latest reply from the LLM.
-  const [reply, setReply] = useState('');
 
-  const [started, setStarted] = useState(false);
-  const [finished, setFinished] = useState(true);
-
-  const { loading, error, value } = useAsync(async () => {
-    // Check if the LLM plugin is enabled and configured.
-    // If not, we won't be able to make requests, so return early.
-    const enabled = await llms.openai.enabled();
-    if (!enabled) {
-      return { enabled };
-    }
-    if (message === '') {
-      return { enabled };
-    }
-
-    setStarted(true);
-    setFinished(false);
-    // Stream the completions. Each element is the next stream chunk.
-    const stream = llms.openai.streamChatCompletions({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        { role: 'system', content: 'You are a cynical assistant.' },
-        { role: 'user', content: message },
-      ],
-    }).pipe(
-      // Accumulate the stream content into a stream of strings, where each
-      // element contains the accumulated message so far.
-      llms.openai.accumulateContent(),
-      // The stream is just a regular Observable, so we can use standard rxjs
-      // functionality to update state, e.g. recording when the stream
-      // has completed.
-      // The operator decision tree on the rxjs website is a useful resource:
-      // https://rxjs.dev/operator-decision-tree.
-      finalize(() => {
-        setStarted(false);
-        setFinished(true);
-      })
-    );
-    // Subscribe to the stream and update the state for each returned value.
-    return {
-      enabled,
-      stream: stream.subscribe(setReply),
-    };
-  }, [message]);
-
-  if (error) {
-    // TODO: handle errors.
-    return null;
-  }
+  const {
+    setMessages,
+    reply,
+    started,
+    finished,
+    loading,
+    error,
+    value
+  } = useOpenaiStreamHook();
 
   return (
     <PluginPage>
@@ -73,7 +28,16 @@ export function ExamplePage() {
             placeholder="Enter a message"
           />
           <br />
-          <Button type="submit" onClick={() => setMessage(input)}>Submit</Button>
+          <Button
+            type="submit"
+            onClick={() => setMessages((oldMessages: llms.openai.Message[]) => [
+              { role: 'system', content: 'You are a cynical assistant.' },
+              { role: 'user', content: input}
+            ])}
+            disabled={(error !== undefined) || loading || started}
+          >
+            Submit
+          </Button>
           <br />
           <div>{loading ? <Spinner /> : reply}</div>
           <div>{started ? "Response is started" : "Response is not started"}</div>
